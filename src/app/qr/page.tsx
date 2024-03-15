0ae83fcd0c8be33583bed52315355cc6f63cc5d6
Rigan Burnwal <therealriganb@gmail.com>
2024-03-15 19:18:22 +0530

    fixing overflow of camera module and front rear issue

--- src/app/qr/page.tsx
+++ src/app/qr/page.tsx
@@ -12,8 +12,12 @@ export default function Home() {
 
   return (
     <div className="flex flex-col md:flex-row h-full gap-5 items-center justify-start p-5">
-      <div className="md:w-1/3 w-full md:h-full h-1/3 bg-blue-500">
-        <QrReader onResult={onScanResult} constraints={{}} scanDelay={1000} />
+      <div className="md:w-1/3 w-full md:h-full h-1/3 bg-blue-500 overflow-hidden">
+        <QrReader
+          onResult={onScanResult}
+          constraints={{ facingMode: "environment" }}
+          scanDelay={1000}
+        />
       </div>
       <div className="flex-1 text-3xl">
         <span className="text-blue-500">Last decoded value:</span> {decodedText}
